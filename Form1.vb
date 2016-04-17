d8f884932a3bfd178256c1d8a5909215ac3daaa7
Benyamin Limanto <me@benyamin.xyz>
2016-04-17 13:41:57 +0700

    A New One
    Try One

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class MinesWordForm
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim TryFelix As New Felix
-        MsgBox(TryFelix.Print())
+        MsgBox(TryFelix.Print() & TryFelix.PrintNa)
     End Sub
 End Class
