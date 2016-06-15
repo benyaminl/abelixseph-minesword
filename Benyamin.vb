21b732d2b4e2ac2d79b83c3d6f7426cd986cbde5
Benyamin Limanto <me@benyamin.xyz>
2016-06-15 10:25:11 +0700

    Start working, I'm use Benyamin.vb to Manipulate the form and otherss

---
+++ Benyamin.vb
@@ -0,0 +1,13 @@
+﻿Public Class Benyamin
+    Friend StageButton(4) As Button
+    Sub Stage()
+        If StageButton IsNot Nothing Then
+
+        End If
+        For i As Integer = 1 To 4
+            StageButton(i) = New Button
+            StageButton(i).Width = 200
+            StageButton(i).Height = 200
+        Next
+    End Sub
+End Class
--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,3 @@
 ﻿Public Class MinesWordForm
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim TryFelix As New Felix
-        MsgBox(TryFelix.Print() & TryFelix.PrintNa)
-        My.Computer.Audio.Play("C:\dw.wav", AudioPlayMode.BackgroundLoop)
-        MsgBox("Ah lah")
-        MsgBox("Ah lah")
-    End Sub
+    
 End Class
