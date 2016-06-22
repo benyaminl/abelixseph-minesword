6fb55ce667266921517dcbe50a264e1d6262bf96
geooos <stevanusfelix.stts@gmail.com>
2016-06-22 10:01:45 +0700

    gabung Seluruhnya, Main menu
    Main menu working with it
    Jadi sudah selesai

--- Form1.vb
+++ Form1.vb
@@ -1,8 +1,10 @@
 ﻿Public Class MinesWordForm
     Dim Ben As New Benyamin 'Importing Class Benyamin, from Benyamin Limanto
+    Dim Felix As New MainMenu
     Private Sub MinesWordForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Reducing Flicker because of button animation
         Me.DoubleBuffered = True
-        Ben.StageButtonShow()
+        Felix.Mainmenupage()
+        'Ben.StageButtonShow()
     End Sub
 End Class
